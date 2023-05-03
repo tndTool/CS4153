ea7b8963795145887fed73357a08a8eb6cc5d850
I0ntdtn0I <19522362@gm.uit.edu.vn>
2023-05-03 17:45:21 +0700

    Fetch Users From Firebase"

---
+++ app/src/main/java/com/cs4153/Adapter/UsersAdapter.java
@@ -0,0 +1,60 @@
+package com.cs4153.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cs4153.Models.Users;
+import com.cs4153.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends  RecyclerView.Adapter<UsersAdapter.ViewHolder> {
+
+    ArrayList<Users> list;
+    Context context;
+
+    public UsersAdapter(ArrayList<Users> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.sample_show_user, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Users users = list.get(position);
+        Picasso.get().load(users.getProfilePic()).placeholder(R.drawable.avatar3).into(holder.image);
+        holder.userName.setText(users.getUserName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView userName, lastMessage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.profile_image);
+            userName = itemView.findViewById(R.id.userNameList);
+            lastMessage = itemView.findViewById(R.id.lastMessage);
+        }
+    }
+}
