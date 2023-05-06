e421bc8af91b7287ed6573541a5518e3feed6bec
I0ntdtn0I <19522362@gm.uit.edu.vn>
2023-05-06 10:40:52 +0700

    Chat and Delete message

--- app/src/main/java/com/cs4153/Adapter/ChatAdapter.java
+++ app/src/main/java/com/cs4153/Adapter/ChatAdapter.java
@@ -1,10 +1,15 @@
 package com.cs4153.Adapter;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,13 +18,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.cs4153.Models.MessageModel;
 import com.cs4153.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ChatAdapter extends RecyclerView.Adapter{
 
     ArrayList<MessageModel> messageModels;
     Context context;
+    String recId;
 
     int SENDER_VIEW_TYPE = 1;
     int RECEIVER_VIEW_TYPE = 2;
@@ -27,6 +36,12 @@ public class ChatAdapter extends RecyclerView.Adapter{
     public ChatAdapter(ArrayList<MessageModel> messageModels, Context context) {
         this.messageModels = messageModels;
         this.context = context;
+    }
+
+    public ChatAdapter(ArrayList<MessageModel> messageModels, Context context, String recId) {
+        this.messageModels = messageModels;
+        this.context = context;
+        this.recId = recId;
     }
 
     @NonNull
@@ -60,13 +75,64 @@ public class ChatAdapter extends RecyclerView.Adapter{
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         MessageModel messageModel = messageModels.get(position);
 
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                AlertDialog alertDialog = new AlertDialog.Builder(context)
+                        .setTitle("Delete")
+                        .setMessage("Are you sure you want to delete this message?")
+                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                String senderRoom = FirebaseAuth.getInstance().getUid() + recId;
+                                database.getReference().child("chats").child(senderRoom)
+                                        .child(messageModel.getMessageId())
+                                        .setValue(null);
+                            }
+                        })
+                        .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .create(); // Create the AlertDialog object
+
+                // Show the AlertDialog
+                alertDialog.show();
+
+                // Get the "yes" button from the AlertDialog's view
+                Button yesButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+
+                // Set the background color and text color of the button
+                yesButton.setBackgroundColor(Color.RED);
+                yesButton.setTextColor(Color.WHITE);
+
+                return false;
+            }
+        });
+
         if (holder.getClass()==SenderViewHolder.class)
         {
             ((SenderViewHolder)holder).senderMsg.setText(messageModel.getMessage());
+
+            Date date = new Date(messageModel.getTimestamp());
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("h:mm a");
+            String strDate = simpleDateFormat.format(date);
+
+            ((SenderViewHolder)holder).senderTime.setText(strDate.toString());
         }
         else
         {
             ((RecieverViewHolder)holder).receiverMsg.setText(messageModel.getMessage());
+
+            Date date = new Date(messageModel.getTimestamp());
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("h:mm a");
+            String strDate = simpleDateFormat.format(date);
+
+            ((RecieverViewHolder)holder).receiverTime.setText(strDate.toString());
         }
     }
 
--- app/src/main/java/com/cs4153/ChatDetailActivity.java
+++ app/src/main/java/com/cs4153/ChatDetailActivity.java
@@ -1,15 +1,26 @@
 package com.cs4153;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.cs4153.Adapter.ChatAdapter;
+import com.cs4153.Models.MessageModel;
 import com.cs4153.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ChatDetailActivity extends AppCompatActivity {
 
@@ -40,6 +51,60 @@ public class ChatDetailActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(ChatDetailActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+        final ChatAdapter chatAdapter = new ChatAdapter(messageModels, this, receiveId);
+
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+        final String senderRoom = senderId + receiveId;
+        final String receiverRoom = receiveId + senderId;
+
+        database.getReference().child("chats")
+                        .child(senderRoom)
+                                .addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        messageModels.clear();
+                                        for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                                            MessageModel model = snapshot1.getValue(MessageModel.class);
+                                            model.setMessageId(snapshot1.getKey());
+                                            messageModels.add(model);
+                                        }
+                                        chatAdapter.notifyDataSetChanged();
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = binding.enterMessage.getText().toString();
+                final MessageModel model = new MessageModel(senderId, message);
+                model.setTimestamp(new Date().getTime());
+                binding.enterMessage.setText("");
+
+                database.getReference().child(("chats")).child(senderRoom).push().setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        database.getReference().child("chats")
+                                .child(receiverRoom).push().setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+
+                                    }
+                                });
+                    }
+                });
             }
         });
     }
